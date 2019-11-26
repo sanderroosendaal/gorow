017165bf540ae8f1471ee2631ec802d9fb7e6e6b
Sander Roosendaal <roosendaalsander@gmail.com>
2019-11-26 20:12:39 +0100

    Implemented all stroke profiles except FromFile

--- crew.go
+++ crew.go
@@ -49,6 +49,90 @@ func (s StrongMiddle) forceprofile(favg, x float64) float64 {
 	return f
 }
 
+// StrongMiddle2 stroke profile, alternative with strong middle
+type StrongMiddle2 struct {
+	frac float64
+}
+
+func (s StrongMiddle2) forceprofile(favg, x float64) float64 {
+	var f = (s.frac * favg * math.Pi * math.Sin(math.Pi*x) / 2.) + 2*(1.-s.frac)*favg*(1-x)
+	return f
+}
+
+// Flat stroke profile
+type Flat struct {
+}
+
+func (s Flat) forceprofile(favg, x float64) float64 {
+	return favg
+}
+
+// Trapezium stroke profile
+type Trapezium struct {
+	h1, h2, x1, x2 float64
+}
+
+func (s Trapezium) forceprofile(favg, x float64) float64 {
+	ratio := s.h1*0.5*s.x2 + s.h2*(0.5-0.5*s.x1)
+	f := 0.0
+	if x < s.x1 {
+		f = favg * s.h1 * x / s.x1
+	} else if x > s.x2 {
+		f = favg * s.h2 * (1. - x) / (1. - s.x2)
+	} else {
+		f = (s.h1 + (s.h2-s.h1)*(x-s.x1)/(s.x2-s.x1)) * favg
+	}
+
+	f = f / ratio
+	return f
+}
+
+// Trapezium2 stroke profile
+type Trapezium2 struct {
+	h0, h1, h2, x1, x2 float64
+}
+
+func (s Trapezium2) forceprofile(favg, x float64) float64 {
+	ratio := s.h1*0.5*s.x2 + s.h2*(0.5-0.5*s.x1) + s.h0
+	ratio2 := s.h1*0.5*s.x2 + s.h2*(0.5-0.5*s.x1)
+	frac := ratio2 / ratio
+	f := 0.0
+	if x < s.x1 {
+		f = favg * s.h1 * x / s.x1
+	} else if x > s.x2 {
+		f = favg * s.h2 * (1. - x) / (1. - s.x2)
+	} else {
+		f = (s.h1 + (s.h2-s.h1)*(x-s.x1)/(s.x2-s.x1)) * favg
+	}
+
+	f = frac*f + (1-frac)*favg*s.h0
+	f = f / ratio
+
+	return f
+}
+
+// FromFile stroke profile class not (yet) implemented
+
+// StrongBegin stroke profile
+type StrongBegin struct {
+	frac float64
+}
+
+func (s StrongBegin) forceprofile(favg, x float64) float64 {
+	f := (2*s.frac*(1.0-x) + (1. - s.frac)) * favg
+	return f
+}
+
+// StrongEnd stroke profile
+type StrongEnd struct {
+	frac float64
+}
+
+func (s StrongEnd) forceprofile(favg, x float64) float64 {
+	f := (2*s.frac*x + (1. - s.frac)) * favg
+	return f
+}
+
 // Crew class with rower quantities
 type Crew struct {
 	mc           float64
@@ -80,11 +164,12 @@ func NewCrew(mc float64, strokelength float64, tempo float64, frac float64,
 	strokeprofile ForceProfile,
 	maxpower float64, maxforce float64) *Crew {
 	return &Crew{
-		mc:           mc,
-		strokelength: strokelength,
-		tempo:        tempo,
-		frac:         frac,
-		maxpower:     maxpower,
-		maxforce:     maxforce,
+		mc:            mc,
+		strokelength:  strokelength,
+		strokeprofile: strokeprofile,
+		tempo:         tempo,
+		frac:          frac,
+		maxpower:      maxpower,
+		maxforce:      maxforce,
 	}
 }
